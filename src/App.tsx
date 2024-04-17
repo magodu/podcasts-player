7c37c976a1593276ef83aa92990c0a7e540ca0c0
Mario Gonzalez Duarte <magodu.pral@gmail.com>
2024-04-17 11:29:07 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+
+    return (
+        <div>
+            <h1>
+                Podcasts project
+            </h1>  
+        </div>
+    );
+}
+
+export default App;
