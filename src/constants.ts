c5164e6563a0e9612bf7eadd9190c558bb46c0a2
Mario Gonzalez Duarte <magodu.pral@gmail.com>
2024-04-19 10:32:32 +0200

    group constants

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const CORS_PROXY = "https://api.allorigins.win/get?url=";
+
+export const PODCASTS_URI = 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
+export const PODCASTS_DETAIL_URI = 'https://itunes.apple.com/lookup?id=PODCAST_ID';
+export const PODCASTS_EPISODES_URI = 'https://itunes.apple.com/lookup?id=PODCAST_ID&media=podcast&entity=podcastEpisode'
+
+export const LOCAL_STORAGE_KEY = 'podcaster';
